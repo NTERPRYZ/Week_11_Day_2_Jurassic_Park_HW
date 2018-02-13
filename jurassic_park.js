00add90b928a0bbadfcaded229fb13adbac59d97
Robert Henderson <robert@nterpryz.com>
2018-02-13 17:20:24 +0000

    can remove dinosaurs

--- jurassic_park.js
+++ jurassic_park.js
@@ -6,4 +6,9 @@ JurassicPark.prototype.addDinosaur = function(dinosaur){
   this.herd.push(dinosaur);
 }
 
+JurassicPark.prototype.removeDinosaur = function(dinosaur){
+  const indexOfItem = this.herd.indexOf(dinosaur)
+  this.herd.splice(indexOfItem, 1);
+}
+
 module.exports = JurassicPark;
--- specs/jurassic_park_specs.js
+++ specs/jurassic_park_specs.js
@@ -5,19 +5,28 @@ const Dinosaur = require("../dinosaur.js")
 describe ("enclosure", function(){
   let enclosure;
   let trex;
+  let tri
 
   beforeEach(function(){
     enclosure = new JurassicPark();
     trex = new Dinosaur("Tyrannosaurus", 2);
+    tri = new Dinosaur("Triceratops", 5);
   });
 
   it("should start of empty", function(){
-    assert.deepStrictEqual(enclosure.herd, []);
+      assert.deepStrictEqual(enclosure.herd, []);
   });
 
   it("should take in dinosaurs", function(){
       enclosure.addDinosaur(trex);
       assert.strictEqual(enclosure.herd.length, 1)
     })
+
+  it("should be able to remove dinosaurs", function(){
+      enclosure.addDinosaur(trex);
+      enclosure.addDinosaur(tri);
+      enclosure.removeDinosaur(trex);
+      assert.strictEqual(enclosure.herd.length, 1)
+  })
 
 });
